971e3f47655f83faa7e38daa4f374d0ed361e5a4
Alexandr Sergeev <rboby.box@mail.ru>
2020-02-11 17:10:40 +0300

    Vector operations return vector

--- Vector2d.cpp
+++ Vector2d.cpp
@@ -17,6 +17,7 @@ Vector2d::Vector2d(const Vector2d& v) {
 	this->x = v.x;
 	this->y = v.y;
 }
+Vector2d::~Vector2d() {};
 void Vector2d::setx(double x) {
 	this->x = x;
 }
@@ -32,17 +33,14 @@ double Vector2d::gety() {
 void Vector2d::print() {
 	std::cout << "(" << x << "; " << y << ")";
 }
-void Vector2d::sum(Vector2d v) {
-	this->x += v.x;
-	this->y += v.y;
+Vector2d* Vector2d::sum(Vector2d v) {
+	return new Vector2d(this->x + v.x, this->y + v.y);
 }
-void Vector2d::sub(Vector2d v) {
-	this->x -= v.x;
-	this->y -= v.y;
+Vector2d* Vector2d::sub(Vector2d v) {
+	return new Vector2d(this->x - v.x, this->y - v.y);
 }
-void Vector2d::mult(double k) {
-	this->x *= k;
-	this->y *= k;
+Vector2d* Vector2d::mult(double k) {
+	return new Vector2d(this->x * k, this->y * k);
 }
 double Vector2d::scalarMult(Vector2d v) {
 	return this->x * v.x + this->y * v.y;
